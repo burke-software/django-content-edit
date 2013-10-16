069fa02b0200485d7fcbe786bc9c4299a1f187ef
David Burke <david@burkesoftware.com>
2013-10-16 19:09:14 -0400

    New pypi version Added MANIFEST.in to git

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-content-edit",
-    version = "1.0",
+    version = "1.1",
     author = "David Burke",
     author_email = "david@burkesoftware.com",
     description = ("A very simple way to let users edit content on the front end of a website."),
