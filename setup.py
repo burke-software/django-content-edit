cd9a238549f5e9db6d80f0a670bb354cdca52bda
Saul Shanabrook <s.shanabrook@gmail.com>
2016-06-15 18:07:06 -0400

    Bump Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-content-edit",
-    version = "1.7",
+    version = "2.0",
     author = "David Burke",
     author_email = "david@burkesoftware.com",
     description = ("A very simple way to let users edit content on the front end of a website."),
